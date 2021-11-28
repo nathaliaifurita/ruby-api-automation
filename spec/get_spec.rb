9ea255bf76599c45e9bd8eec96d6899b495aaf70
nathaliaifurita <nathaliaofreire@gmail.com>
2021-11-28 11:26:11 -0300

    Fixing the lastname and age attributes

--- spec/get_spec.rb
+++ spec/get_spec.rb
@@ -5,15 +5,16 @@ describe 'get' do
     it 'returns a list of contacts' do
       response = HTTParty.get('https://api-de-tarefas.herokuapp.com/contacts/337')
       parsed_response = JSON.parse(response.body)
+      puts parsed_response
       expect(response.code).to eq 200
       expect(response.message).to eql 'OK'
       expect(parsed_response['data']['id']).not_to be_empty
       expect(parsed_response['data']['type']).not_to be_empty
       expect(parsed_response['data']['attributes']).not_to be_empty
       expect(parsed_response['data']['attributes']['name']).not_to be_empty
-      expect(parsed_response['data']['attributes']['lastname']).not_to be_empty
+      expect(parsed_response['data']['attributes']['last-name']).not_to be_empty
       expect(parsed_response['data']['attributes']['email']).not_to be_empty
-      expect(parsed_response['data']['attributes']['age']).not_to be_empty
+      expect(parsed_response['data']['attributes']['age']).not_to be_nil
       expect(parsed_response['data']['attributes']['phone']).not_to be_empty
       expect(parsed_response['data']['attributes']['address']).not_to be_empty
       expect(parsed_response['data']['attributes']['state']).not_to be_empty
